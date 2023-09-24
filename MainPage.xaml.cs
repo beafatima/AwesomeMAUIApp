1e798fc690695f88f6ac62a682042e09130d286b
beafatima <126442264+beafatima@users.noreply.github.com>
2023-09-24 13:49:07 +0800

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+
+namespace AwesomeMAUIApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+        private void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            DisplayLbl.Text = e.NewTextValue;
+        }
+    }
+}
